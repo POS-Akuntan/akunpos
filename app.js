5d6d48582b9959b52a9920e740cf635c103534a6
waskithocitoadiwiguno <waskithocito.a@gmail.com>
2024-12-02 02:08:03 +0700

    Cors *

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Konfigurasi CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["GET", "POST", "PUT", "OPTIONS", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 
 
 // Middleware
